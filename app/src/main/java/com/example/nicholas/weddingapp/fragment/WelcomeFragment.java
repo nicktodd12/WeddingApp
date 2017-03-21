64d5665e93f3c41cf9d4944c53323f1e9837c274
Nicholas <todd.134@osu.edu>
2017-03-20 21:05:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nicholas/weddingapp/fragment/WelcomeFragment.java
@@ -0,0 +1,95 @@
+package com.example.nicholas.weddingapp.fragment;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.nicholas.weddingapp.R;
+import com.example.nicholas.weddingapp.activity.MainActivity;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WelcomeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WelcomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WelcomeFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static WelcomeFragment newInstance() {
+        WelcomeFragment fragment = new WelcomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((MainActivity)getActivity()).setActionBarTitle("Welcome");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_welcome, container, false);
+        Typeface typeface= Typeface.createFromAsset(getActivity().getAssets(), "fonts/OpenSans-Light.ttf");
+        TextView myTextView = (TextView)v.findViewById(R.id.textView1);
+        myTextView.setTypeface(typeface);
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onWelcomeFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onWelcomeFragmentInteraction(Uri uri);
+    }
+}
